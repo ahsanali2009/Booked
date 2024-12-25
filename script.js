b7db05af10309cf7ec385bda412fd185a589a5a6
Ahsan Ali <ahsanali19112009@gmail.com>
2024-12-25 22:54:18 +0500

    extension structure change

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-let container = document.getElementById("container") 
-
-    chrome.bookmarks.getTree()
-    .then(bookmarkDOM => {
+chrome.bookmarks.getTree()
+.then(bookmarkDOM => {
     
-    let bookmarksArray = bookmarkDOM[0].children[0].children
+    let bookmarksArray = bookmarkDOM[0].children[0].children ;
     
-    // bookmarksArray.forEach(bookmark => {
-
-    //     let bookmarkCreated = document.createElement('a')
-    //     bookmarkCreated.href = bookmark.url
-    //     bookmarkCreated.textContent =  bookmark.title
-    //     bookmarkCreated.setAttribute("id","bookmarkCreated")
-    //     container.append(bookmarkCreated)
-    // });
-})
+});
