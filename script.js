4f4df60bedaae07647bfaa844270281c8d5fe8f7
Ahsan Ali <ahsanali19112009@gmail.com>
2024-12-27 14:06:45 +0500

    getting url-favicon fixed

--- script.js
+++ script.js
@@ -25,9 +25,16 @@ try {
             bookmark_input.type = "checkbox";
             bookmark_input.setAttribute('class', 'bookmark-input');
 
+            //Get favicon of the url.
             let bookmark_favicon = document.createElement('img');
             bookmark_favicon.setAttribute('class', 'bookmark-favicon');
-            bookmark_favicon.src = `${bookmarkCreated.url}/favicon.ico`;
+            
+            let bookmarkCreated_http_replace = (bookmarkCreated.url).replace("https://","")
+            let getIndexOfUrl = (bookmarkCreated_http_replace).indexOf("/")
+            let bookmarkCreated_slice = bookmarkCreated_http_replace.slice(0, getIndexOfUrl)
+            let bookmarkCreated_combine = `http://${bookmarkCreated_slice}/favicon.ico`
+            bookmark_favicon.src = `${bookmarkCreated_combine}`;
+
 
             let bookmark_title = document.createElement('div');
             bookmark_title.setAttribute('class', 'bookmark-title');
@@ -35,10 +42,10 @@ try {
             let p_title = document.createElement('p');
             p_title.textContent = (bookmarkCreated.title)
 
-        if((bookmarkCreated.title).length > 49){ // For getting the short title
+        if((bookmarkCreated.title).length > 49){ // Get short title.
             let temp_p_title = (bookmarkCreated.title).slice(0,49)  
             p_title.textContent = `${temp_p_title} ....`        
-        }else {
+        } else {
              p_title.textContent = (bookmarkCreated.title) }
 
 
