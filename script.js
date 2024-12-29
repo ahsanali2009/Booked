863fc79f8bd21fa96b054671774b37770506d864
Ahsan Ali <ahsanali19112009@gmail.com>
2024-12-29 15:15:55 +0500

    search functionallity done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 try {
     chrome.bookmarks.getTree(function (bookmarkDOM) {
-        // Find the bookmarks array
+
         let bookmarksArray = bookmarkDOM[0].children[0]?.children;
 
         if (!bookmarksArray) {
             console.error("No bookmarks found.");
-            return ;
+            return;
         }
 
         let container = document.getElementById('bookmarks-container');
@@ -16,64 +16,75 @@ try {
             document.body.appendChild(container);
         }
 
-        bookmarksArray.forEach((bookmarkCreated) => {
 
-            let bookmark = document.createElement('div');
-            bookmark.setAttribute('class', 'bookmark');
+        function renderBookmarks(bookmarks) {
+            container.innerHTML = '';
+            bookmarks.forEach((bookmarkCreated) => {
+                let bookmark = document.createElement('div');
+                bookmark.setAttribute('class', 'bookmark');
 
-            let bookmark_input = document.createElement('input');
-            bookmark_input.type = "checkbox";
-            bookmark_input.setAttribute('class', 'bookmark-input');
+                let bookmark_input = document.createElement('input');
+                bookmark_input.type = "checkbox";
+                bookmark_input.setAttribute('class', 'bookmark-input');
 
-            //Get favicon of the url.
-            let bookmark_favicon = document.createElement('img');
-            bookmark_favicon.setAttribute('class', 'bookmark-favicon');
-            
-            let bookmarkCreated_http_replace = (bookmarkCreated.url).replace("https://","")
-            let getIndexOfUrl = (bookmarkCreated_http_replace).indexOf("/")
-            let bookmarkCreated_slice = bookmarkCreated_http_replace.slice(0, getIndexOfUrl)
-            let bookmarkCreated_combine = `http://${bookmarkCreated_slice}/favicon.ico`
-            
-            bookmark_favicon.src = bookmarkCreated_combine;
+                
+                let bookmark_favicon = document.createElement('img');
+                bookmark_favicon.setAttribute('class', 'bookmark-favicon');
 
+                let bookmarkCreated_http_replace = (bookmarkCreated.url).replace("https://", "")
+                let getIndexOfUrl = (bookmarkCreated_http_replace).indexOf("/")
+                let bookmarkCreated_slice = bookmarkCreated_http_replace.slice(0, getIndexOfUrl)
+                let bookmarkCreated_combine = `http://${bookmarkCreated_slice}/favicon.ico`
 
-            let bookmark_title = document.createElement('div');
-            bookmark_title.setAttribute('class', 'bookmark-title');
+                bookmark_favicon.src = bookmarkCreated_combine;
 
-            let p_title = document.createElement('a');
-            p_title.href = bookmarkCreated.url
-            p_title.textContent = (bookmarkCreated.title)
+                let bookmark_title = document.createElement('div');
+                bookmark_title.setAttribute('class', 'bookmark-title');
 
-            if ((bookmarkCreated.title).length > 49) { // Get short title.
-                let temp_p_title = (bookmarkCreated.title).slice(0,49)  
-                p_title.textContent = `${temp_p_title} ....`        
+                let p_title = document.createElement('a');
+                p_title.href = bookmarkCreated.url
+                p_title.textContent = (bookmarkCreated.title)
 
-            } else {
-             p_title.textContent = (bookmarkCreated.title) }
+                if ((bookmarkCreated.title).length > 49) { // Get short title.
+                    let temp_p_title = (bookmarkCreated.title).slice(0, 49)
+                    p_title.textContent = `${temp_p_title} ....`
+                } else {
+                    p_title.textContent = (bookmarkCreated.title)
+                }
 
-            bookmark.append(bookmark_input);
-            bookmark.append(bookmark_favicon);
-            bookmark.append(bookmark_title);
-            bookmark_title.appendChild(p_title);
-            container.appendChild(bookmark);
-            
+                bookmark.append(bookmark_input);
+                bookmark.append(bookmark_favicon);
+                bookmark.append(bookmark_title);
+                bookmark_title.appendChild(p_title);
+                container.appendChild(bookmark);
+
                 function openUrl(element) {
                     element.onclick = () => {
                         window.open(bookmarkCreated.url)
                     }
                 }
+
                 openUrl(bookmark_title)
                 openUrl(bookmark_favicon)
+            });
+        }
 
 
-            let bookmarkDivsArray = []
-            bookmarkDivsArray.push(bookmark) // Add bookmarks to the bookmarkDivs Array.
-            
+        renderBookmarks(bookmarksArray);
 
-        })
-        
+
+        let searchBar = document.getElementById("searchBar");
+        searchBar.oninput = () => {
+            let query = searchBar.value.toLowerCase();
+            let filteredBookmarksArray = bookmarksArray.filter((bookmark) => {
+                return bookmark.title.toLowerCase().includes(query);
+            });
+
+
+            renderBookmarks(filteredBookmarksArray);
+        }
     });
-    
+
 } catch (error) {
     console.log(error)
 }
